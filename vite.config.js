9e3c5c89800e3ef37b4b8035bbd4693a235ccac8
shivaramudu <shivaramudu2002@gmail.com>
2024-10-04 16:13:13 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy/"
-  // base: '/Honey_Online_Website/', // Comment out or remove this line for local development
+  base: '/ECommerse_App/', // Make sure this matches your GitHub repo name
 })
